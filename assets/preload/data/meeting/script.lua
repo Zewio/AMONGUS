df158f8762ce0d7cd92182e0f1b473170950f611
Zewio <121409748+Zewio@users.noreply.github.com>
2023-01-06 20:05:13 -0300

    Add files via upload

---
+++ assets/preload/data/meeting/script.lua
@@ -0,0 +1,5 @@
+function onCreate() 
+makeLuaText('watermark',"Ported By zpoiklz ", 0, 2, 688); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 30);
+    addLuaText('watermark');
+end
